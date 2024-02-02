02ea6eafd1c7ddfe207c8ffbb3e179ae817288c1
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-02 18:40:45 +0000

    Updated package.json name field, updated generatorMarkdown.js with function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,48 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Credits](#credits)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  * [License](#license)
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation Instructions
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ![License badge](https://img.shields.io/badge/license-${data.license}-brightgreen).
+
+  ## Contributing
+
+  ${data.contribute}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## Questions
+
+  If you have further questions, you can reach me at ${data.email}. For more of my work, see [my GitHub](https://github.com/${data.github}).
 `;
 }
 
